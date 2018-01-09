6f226dc14f69fbe7c23e820036b94aafa7a2ced7
sdob <sdob@cs.ucc.ie>
2018-01-09 10:23:53 +0000

    Remove logging

--- src/context/actions.js
+++ src/context/actions.js
@@ -2,8 +2,6 @@ export const SET_WINDOW_DIMENSIONS = 'context/SET_WINDOW_DIMENSIONS';
 
 function setWindowDimensions(width, height) {
   return (dispatch) => {
-    console.info('width: ' + width);
-    console.info('height' + height);
     dispatch({ type: SET_WINDOW_DIMENSIONS, payload: { width, height } });
   };
 }
